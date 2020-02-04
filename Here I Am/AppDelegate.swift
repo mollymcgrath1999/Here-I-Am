381970f49012046a10c2dae7a2bfba6383c09a84
mollymcgrath1999 <59444202+mollymcgrath1999@users.noreply.github.com>
2020-02-04 14:27:31 +0000

    Removed comments

--- Here I Am/AppDelegate.swift
+++ Here I Am/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Here I Am
-//
-//  Created by Molly McGrath (s5008390) on 03/02/2020.
-//  Copyright © 2020 Molly McGrath (s5008390). All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
