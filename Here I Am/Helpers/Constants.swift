4481b3cc4355e0b8955b4d900a2166e8f9a255f5
Molly McGrath (s5008390) <s5008390@mac42940.mac.bournemouth.ac.uk>
2020-02-07 17:04:52 +0000

    authentication login

---
+++ Here I Am/Helpers/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  Here I Am
+//
+//  Created by Molly McGrath (s5008390) on 07/02/2020.
+//  Copyright © 2020 Molly McGrath (s5008390). All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    struct Storyboard {
+        
+      static  let homeViewController = "HomeVC"
+        
+        
+    }
+    
+    
+    
+}
--- Here I Am/SignUpViewController.swift
+++ Here I Am/SignUpViewController.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
+
 
 class SignUpViewController: UIViewController {
     
@@ -62,11 +66,26 @@ class SignUpViewController: UIViewController {
         if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
            lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+            
             {
                 
-                return "Please fill out all Fields" }
+                return "Please fill out all Fields"
+                
+                
+                
+        }
        
+        // Check if the password is secure
+        
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            
+            // Password isn't secure
+            
+            return "Please make sure your password is at least 8 characters, contains a special character and a number"
+        }
         
         return nil 
     }
@@ -97,16 +116,83 @@ class SignUpViewController: UIViewController {
     
     
     @IBAction func signUpTapped(_ sender: Any) {
+        
+        func showError(_message:String) {
+            
+            errorLabel.text = _message
+            errorLabel.alpha = 1
+        }
+        
         // Validate the fields
+        let error = validateFields()
+        if error != nil {
+            
+            // There's something wrong with the fields, show error message
+            
+            showError(_message: error!)
+        }
         
-        // Create the user
-        
-        // Transition to the home screen
-        
+        else {
+            
+            // Create cleaned versions of the data
+            let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            // Create the user
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                
+                
+            // Check for errors
+                if err != nil {
+                
+                    if err != nil {
+                    // There was an error creating the user
+                    showError(_message: " Error creating user")
+                }
+                        
+                else {
+                    // User was created successfully, now sotre the first name and last name
+                        let db = Firestore.firestore()
+                        db.collection("users").addDocument(data:["firstname":firstName, "lastname":lastName, "uid":result!.user.uid]) { (error) in
+                            
+                            if error != nil {
+                                // Show error message
+                                showError(_message:"Error saving user data")
+                                
+                            }
+                        }
+                    
+                    // Transistion to the home screen
+                        self.transitionToHome()
+                    
+                    }
+                    
+                    
+                    
+                    
+            }
+           
+            
+            
+     
         
     }
     
+  
     
     
+
+}
+}
+    func transitionToHome() {
+        let homeViewController =
+            
+        storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController) as? HomeViewController
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+
+    }
 
 }
