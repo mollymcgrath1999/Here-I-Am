cde7a64c36c899a6e3d473a68af97d1e1399b78b
Molly McGrath (s5008390) <s5008390@mac42940.mac.bournemouth.ac.uk>
2020-02-10 09:49:12 +0000

    addedlogindetaild

--- Here I Am/HomeViewController.swift
+++ Here I Am/HomeViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Here I Am/LoginViewController.swift
+++ Here I Am/LoginViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
     
-    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var emailNameTextField: UITextField!
     
     
-    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var passwordNameTextField: UITextField!
     
     
     @IBOutlet weak var loginButton: UIButton!
@@ -47,19 +48,42 @@ class LoginViewController: UIViewController {
         
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
     
     
     @IBAction func loginTapped(_ sender: Any) {
+        
+        
+        // TODO: Validate Text Fields
+        
+        //Create cleaned versions of the text field
+        let email = emailNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Signing in the user
+        Auth.auth().signIn(withEmail: email, link: password) { (result, error) in
+            
+            if error != nil {
+                //Couldn't sign up
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            }
+            else {
+                let homeViewController =
+                          
+                    self.storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                self.view.window?.rootViewController = homeViewController
+                self.view.window?.makeKeyAndVisible()
+                
+                
+                
+            }
+        }
+        
     }
+    
+    
+    
     
 
 }
