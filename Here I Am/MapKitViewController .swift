6ae07624aaf8043618947d9c870c5b00b27416bd
Molly McGrath (s5008390) <s5008390@mac42947.mac.bournemouth.ac.uk>
2020-02-20 17:09:47 +0000

    setting up mapkit settings

--- Here I Am/MapKitViewController .swift
+++ Here I Am/MapKitViewController .swift
@@ -4,6 +4,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 
 class MapKitViewController: UIViewController  {
@@ -11,5 +12,29 @@ class MapKitViewController: UIViewController  {
     @IBOutlet weak var MapKit: MKMapView!
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled(){
+            // setup our location manager
+            
+        } else {
+            //
+        }
+        
+        
+    }
+    
+    
+}
+
+
+extension MKMapView: CLLocationManagerDelegate {
+    
+    public func locationManager(_ _manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
 }
 
