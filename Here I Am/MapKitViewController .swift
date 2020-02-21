1f9e1fefff4e8dfdbe32511fbd827e9d922b69e2
Molly McGrath (s5008390) <s5008390@mac42947.mac.bournemouth.ac.uk>
2020-02-21 16:49:22 +0000

    mapkit settings applied

--- Here I Am/MapKitViewController .swift
+++ Here I Am/MapKitViewController .swift
@@ -11,24 +11,82 @@ class MapKitViewController: UIViewController  {
     
     @IBOutlet weak var MapKit: MKMapView!
     
+    let locationManager = CLLocationManager()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    func checkLocationServices() {
-        if CLLocationManager.locationServicesEnabled(){
-            // setup our location manager
-            
-        } else {
-            //
-        }
+    
+    func setupLocoationManager() {
+     
+        locationManager.delegate = self as? CLLocationManagerDelegate
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
         
         
     }
     
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled(){
+          setupLocoationManager()
+            
+        } else {
+            // Show alert letting the user know they have to turn this on.
+        }
+        
+        
+    }
+    func checkLocationAuthorization() {
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocoationManager()
+            checkLocationAuthorization()
+            
+        } else {
+            
+            //Show alert letting the user know they have to turn this on.
+        }
+            
+            
+        }
     
 }
+    
+func checkLocationAuthorization() {
+    
+    switch CLLocationManager.authorizationStatus() {
+    case.authorizedWhenInUse: break
+        // Do Map stuff
+        
+    case.denied: break
+    // Show alert instructing them how to turn on permissions
+        
+    case.notDetermined: break
+    
+        
+        
+        
+        
+        
+        
+    case .restricted:
+        <#code#>
+    case .authorizedAlways:
+        <#code#>
+    @unknown default:
+        <#code#>
+    }
+    
+    
+    
+}
+    
+    
+
+    
+    
+
 
 
 extension MKMapView: CLLocationManagerDelegate {
